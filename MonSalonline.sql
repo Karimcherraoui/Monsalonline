7e5bf754cf5d8bbb653625bab3915cf71ce880a5
karimcherraoui <cherraoui.karim96@gmail.com>
2023-04-07 14:49:39 +0000

    new updates

--- MonSalonline.sql
+++ MonSalonline.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 01, 2023 at 10:08 PM
+-- Generation Time: Apr 07, 2023 at 02:49 PM
 -- Server version: 8.0.32-0ubuntu0.22.04.2
--- PHP Version: 8.1.2-1ubuntu2.10
+-- PHP Version: 8.1.2-1ubuntu2.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,9 +31,21 @@ CREATE TABLE `clients` (
   `id` int NOT NULL,
   `nom` varchar(255) DEFAULT NULL,
   `prenom` varchar(255) DEFAULT NULL,
-  `numero_tele` int DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `phone` int DEFAULT NULL,
   `reference` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `nom`, `prenom`, `email`, `phone`, `reference`) VALUES
+(22, 'Quaerat in magni ut ', 'Nemo deserunt sed ut', 'mafit@mailinator.com', 123456, 'Jwc9VwR1'),
+(23, 'Praesentium iure con', 'Quidem sit anim labo', 'hynajuse@mailinator.com', 1265456, 'NMMt1NI0'),
+(24, 'Molestias dolorem es', 'Id provident ut sed', 'xahyp@mailinator.com', 78956225, '9R88tQRd'),
+(25, 'Consequatur mollit c', 'Est consectetur et v', 'xulamoq@mailinator.com', 1186, 'wx9AQodA'),
+(26, 'In quod autem debiti', 'Qui perferendis anim', 'vatygy@mailinator.com', 9865454, 'oVFxYcF1');
 
 -- --------------------------------------------------------
 
@@ -47,7 +59,8 @@ CREATE TABLE `horaire` (
   `Heure_douverture_matin` varchar(25) NOT NULL,
   `Heure_de_fermeture_matin` varchar(25) NOT NULL,
   `Heure_douverture_après_midi` varchar(25) NOT NULL,
-  `Heure_de_fermeture_après_midi` varchar(25) NOT NULL
+  `Heure_de_fermeture_après_midi` varchar(25) NOT NULL,
+  `available_hours` int GENERATED ALWAYS AS (((`Heure_de_fermeture_matin` - `Heure_douverture_matin`) + ifnull((`Heure_de_fermeture_après_midi` - `Heure_douverture_après_midi`),0))) VIRTUAL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -72,11 +85,21 @@ INSERT INTO `horaire` (`id`, `jour`, `Heure_douverture_matin`, `Heure_de_fermetu
 CREATE TABLE `rdvs` (
   `id` int NOT NULL,
   `id_client` int DEFAULT NULL,
-  `dates` int DEFAULT NULL,
-  `heure` varchar(25) DEFAULT NULL,
-  `statut` varchar(25) DEFAULT NULL,
-  `reference` varchar(25) DEFAULT NULL
+  `jour` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `heure` varchar(255) NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `statut` varchar(25) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `rdvs`
+--
+
+INSERT INTO `rdvs` (`id`, `id_client`, `jour`, `heure`, `date`, `statut`) VALUES
+(53, 23, 'Thursday', '9h - 10h', '2023-04-06', 'en cours'),
+(54, 23, 'Friday', '9h - 10h', '2023-04-07', 'en cours'),
+(55, 25, 'Thursday', '10h - 11h', '2023-04-06', 'en cours'),
+(56, 25, 'Friday', '11h - 12h', '2023-04-07', 'en cours');
 
 --
 -- Indexes for dumped tables
@@ -109,7 +132,7 @@ ALTER TABLE `rdvs`
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT for table `horaire`
@@ -121,7 +144,7 @@ ALTER TABLE `horaire`
 -- AUTO_INCREMENT for table `rdvs`
 --
 ALTER TABLE `rdvs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- Constraints for dumped tables
